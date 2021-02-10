fa89cebc790749e7b7cde0fd73cae87987bd95a5
liakhivan <liakhivan21@gmail.com>
2021-02-10 23:33:42 +0200

    Was learnt working with static files.

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,18 +25,23 @@ namespace HelloApp
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+           // DefaultFilesOptions options = new DefaultFilesOptions();
+            //options.DefaultFileNames.Clear();
+           // options.DefaultFileNames.Add("hello.html");
+
+            //app.UseDefaultFiles(options);
+            //app.UseDirectoryBrowser();
+            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")), 
+
+            //    RequestPath = new PathString("/pages")
+            //});
+            //app.UseStaticFiles();
+            app.UseFileServer(enableDirectoryBrowsing: true);
             app.Run(async (context) =>
             {
-                context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
-
-                if(env.IsEnvironment("Test"))
-                {
-                    await context.Response.WriteAsync("Is testing");
-                }
-                else
-                {
-                    await context.Response.WriteAsync("I don't know -\\_(*_*)_/-");
-                }
+                await context.Response.WriteAsync("Hello world!");
             });
         }
     }
