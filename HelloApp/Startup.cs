34fa9e351abf703ede436c99e181785fa89b7ba0
liakhivan <liakhivan21@gmail.com>
2021-02-10 22:37:21 +0200

    Was learnt Data pipeline

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -14,6 +14,7 @@ namespace HelloApp
     {
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             app.UseMiddleware<AutentificationMiddleware>();
             app.UseMiddleware<RoutingMiddleware>();
         }
